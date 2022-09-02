2a9faf5cf64633a63331638e8f76b3dc6123ae9e
Jotape-M <joaopedro.m2507@gmail.com>
2022-09-02 16:30:42 -0300

    first commit

---
+++ src/main/java/com/jotapem/youraccount/YourAccountApplication.java
@@ -0,0 +1,13 @@
+package com.jotapem.youraccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourAccountApplication.class, args);
+	}
+
+}
