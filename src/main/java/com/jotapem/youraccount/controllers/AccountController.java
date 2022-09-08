d8c1fc4fc83b6240217b9ac9fd175b61a74d714d
Jotape-M <joaopedro.m2507@gmail.com>
2022-09-08 10:47:45 -0300

    refactor: add annotation required args constructor

--- src/main/java/com/jotapem/youraccount/controllers/AccountController.java
+++ src/main/java/com/jotapem/youraccount/controllers/AccountController.java
@@ -3,7 +3,9 @@ package com.jotapem.youraccount.controllers;
 import com.jotapem.youraccount.models.dto.AccountCreateDTO;
 import com.jotapem.youraccount.models.entities.Account;
 import com.jotapem.youraccount.services.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +14,10 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/accounts")
+@RequiredArgsConstructor
 public class AccountController {
 
     private final AccountService accountService;
-
-    public AccountController(AccountService accountService) {
-        this.accountService = accountService;
-    }
 
     public ResponseEntity<Account> create(@RequestBody @Valid AccountCreateDTO account) {
          return ResponseEntity.ok(accountService.create(account));
--- src/main/java/com/jotapem/youraccount/services/impl/AccountServiceImpl.java
+++ src/main/java/com/jotapem/youraccount/services/impl/AccountServiceImpl.java
@@ -5,20 +5,17 @@ import com.jotapem.youraccount.models.dto.AccountCreateDTO;
 import com.jotapem.youraccount.models.entities.Account;
 import com.jotapem.youraccount.repositories.AccountRepository;
 import com.jotapem.youraccount.services.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
 
     private final AccountRepository accountRepository;
 
     private final AccountMapper accountMapper;
-
-    public AccountServiceImpl(AccountRepository accountRepository, AccountMapper accountMapper) {
-        this.accountRepository = accountRepository;
-        this.accountMapper = accountMapper;
-    }
 
     @Override
     public Account create(AccountCreateDTO account) {
