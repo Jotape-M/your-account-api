8ff0e4d6832a2b236526415c664676d57acee0ed
Jotape-M <joaopedro.m2507@gmail.com>
2022-09-11 11:21:40 -0300

    fix: fix the creation of accounts

--- src/main/java/com/jotapem/youraccount/mappers/AccountMapper.java
+++ src/main/java/com/jotapem/youraccount/mappers/AccountMapper.java
@@ -7,5 +7,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
 
-    Account toModel(AccountCreateDTO account);
+    Account toModel(AccountCreateDTO accountCreateDTO);
 }
---
+++ src/main/resources/db/migration/V03__add_update_and_create_columns.sql
@@ -0,0 +1,2 @@
+ALTER TABLE accounts ADD created_at date;
+ALTER TABLE accounts ADD updated_at date;
