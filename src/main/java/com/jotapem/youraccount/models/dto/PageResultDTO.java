461245c3e6cf6bcd7b6d4d756330ba3994216401
Jotape-M <joaopedro.m2507@gmail.com>
2022-09-16 16:39:54 -0300

    docs: add page result documentation

--- src/main/java/com/jotapem/youraccount/models/dto/PageResultDTO.java
+++ src/main/java/com/jotapem/youraccount/models/dto/PageResultDTO.java
@@ -1,30 +1,41 @@
 package com.jotapem.youraccount.models.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 @Getter
+@Schema(description = "Object stores information from a list of elements per page.")
 public class PageResultDTO<T> {
 
+    @Schema(description = "Total Records.", example = "56")
     private final long totalElements;
 
+    @Schema(description = "Total pages.", example = "3")
     private final int totalPages;
 
+    @Schema(description = "Number of records.", example = "20")
     private final int numberElements;
 
+    @Schema(description = "Number of records per page.", example = "20")
     private final int pageSize;
 
+    @Schema(description = "Page number.", example = "0")
     private final int pageNumber;
 
+    @Schema(description = "It is the last record.", example = "false")
     private final boolean last;
 
+    @Schema(description = "It is the first record.", example = "true")
     private final boolean first;
 
+    @Schema(description = "It is empty.", example = "false")
     private final boolean empty;
 
+    @Schema(description = "List of elements.")
     private final List<T> elements;
 
     public PageResultDTO(Page<T> page) {
