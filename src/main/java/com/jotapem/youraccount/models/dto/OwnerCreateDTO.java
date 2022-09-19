3d09aa68e17772224e7a0406cd0005d06aa39a0c
Jotape-M <joaopedro.m2507@gmail.com>
2022-09-19 17:18:37 -0300

    docs: add owner create dto documentation

--- src/main/java/com/jotapem/youraccount/models/dto/OwnerCreateDTO.java
+++ src/main/java/com/jotapem/youraccount/models/dto/OwnerCreateDTO.java
@@ -1,6 +1,7 @@
 package com.jotapem.youraccount.models.dto;
 
 import com.jotapem.youraccount.validations.constraints.OfAgeConstraint;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,29 +10,36 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@Schema(description = "Object used to create the owner.")
 public class OwnerCreateDTO {
 
     @NotBlank
     @Size(max = 40)
+    @Schema(description = "Owner's first name.", example = "Michael")
     private String firstName;
 
     @NotBlank
     @Size(max = 40)
+    @Schema(description = "Owner's last name.", example = "Ross")
     private String lastName;
 
     @NotBlank
     @Size(max = 40)
+    @Schema(description = "Owner's nickname.", example = "Mike")
     private String nickname;
 
     @NotNull
     @OfAgeConstraint
+    @Schema(description = "Owner's birth date.", example = "2001-02-03")
     private LocalDate birthDate;
 
     @Email
     @Size(max = 60)
+    @Schema(description = "Owner's email.", example = "mike007@gmail.com", required = true)
     private String email;
 
     @NotBlank
     @Size(max = 14)
+    @Schema(description = "Owner's telephone.", example = "85990907755")
     private String telephone;
 }
