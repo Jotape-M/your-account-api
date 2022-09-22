c5b71a8f864cd04066787a0c8374468253fc219e
Jotape-M <joaopedro.m2507@gmail.com>
2022-09-22 15:48:44 -0300

    docs: add documentation delete account operation

--- src/main/java/com/jotapem/youraccount/controllers/docs/AccountControllerDocs.java
+++ src/main/java/com/jotapem/youraccount/controllers/docs/AccountControllerDocs.java
@@ -41,5 +41,11 @@ public interface AccountControllerDocs {
     void update(UUID id, AccountUpdateDTO account);
 
 
+    @Operation(summary = "Delete account operation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Client error",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Account not found")
+    })
     void deleteById(UUID id);
 }
