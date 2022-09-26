bb8d8cd6437db94ffc1ff7a9401cc8b11de9547f
Jotape-M <joaopedro.m2507@gmail.com>
2022-09-26 11:40:32 -0300

    docs: add documentation account create dto

--- src/main/java/com/jotapem/youraccount/models/dto/account/AccountCreateDTO.java
+++ src/main/java/com/jotapem/youraccount/models/dto/account/AccountCreateDTO.java
@@ -12,12 +12,15 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Schema(description = "Object used to create the account.")
 public class AccountCreateDTO {
 
     @Size(min = 4, max = 4)
+    @Schema(description = "Account's agency.", example = "1360")
     private String agency;
 
     @NotNull
+    @Schema(description = "Account's balance.", example = "65.90")
     private BigDecimal balance;
 
     @Valid
