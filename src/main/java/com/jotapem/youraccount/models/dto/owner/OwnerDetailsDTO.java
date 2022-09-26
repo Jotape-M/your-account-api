bac5b231ee5670409fb5427c8318be4ba63419a5
Jotape-M <joaopedro.m2507@gmail.com>
2022-09-26 15:01:54 -0300

    docs: add documentation owner details dto

--- src/main/java/com/jotapem/youraccount/models/dto/owner/OwnerDetailsDTO.java
+++ src/main/java/com/jotapem/youraccount/models/dto/owner/OwnerDetailsDTO.java
@@ -1,5 +1,6 @@
 package com.jotapem.youraccount.models.dto.owner;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,12 +9,27 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Schema(description = "Object used to view the owner.")
 public class OwnerDetailsDTO {
+
+    @Schema(description = "Owner's id.")
     private UUID id;
+
+    @Schema(description = "Owner's first name.", example = "Michael")
     private String firstName;
+
+    @Schema(description = "Owner's last name.", example = "Ross")
     private String lastName;
+
+    @Schema(description = "Owner's nickname.", example = "Mike")
     private String nickname;
+
+    @Schema(description = "Owner's birth date.", example = "2001-02-03")
     private LocalDate birthDate;
+
+    @Schema(description = "Owner's email.", example = "mike007@gmail.com")
     private String email;
+
+    @Schema(description = "Owner's telephone.", example = "85990907755")
     private String telephone;
 }
