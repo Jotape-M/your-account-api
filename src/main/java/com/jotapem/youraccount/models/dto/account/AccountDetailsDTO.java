27b58135fb2768699ea5d18c6d9a5eccacc8ef2a
Jotape-M <joaopedro.m2507@gmail.com>
2022-09-26 14:57:46 -0300

    docs: add documentation account details dto

--- src/main/java/com/jotapem/youraccount/models/dto/account/AccountDetailsDTO.java
+++ src/main/java/com/jotapem/youraccount/models/dto/account/AccountDetailsDTO.java
@@ -1,6 +1,7 @@
 package com.jotapem.youraccount.models.dto.account;
 
 import com.jotapem.youraccount.models.dto.owner.OwnerDetailsDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,11 +11,23 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Schema(description = "Object used to view the account.")
 public class AccountDetailsDTO {
+
+    @Schema(description = "Account's id.")
     private UUID id;
+
+    @Schema(description = "Account's number.", example = "098713235-1")
     private String number;
+
+    @Schema(description = "Account's agency.", example = "0916")
     private String agency;
+
+    @Schema(description = "Account's balance.", example = "1350.00")
     private BigDecimal balance;
+
+    @Schema(description = "Account's creation date.", example = "2022-10-01")
     private LocalDateTime createdAt;
+
     private OwnerDetailsDTO owner;
 }
